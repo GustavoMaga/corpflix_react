6481c5d1a1a6e16e6d467fd7e196d3c4fcfa6d7d
unknown <gustaestudos@gmail.com>
2020-07-31 16:18:07 -0300

    ERROR: Dependencess the git - finished

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
+
+//Importing the components which pages will used
 import PageDefault from '../../../components/template/Base'
+
+//Importing function to routing the page
+import { Link } from 'react-router-dom'
 
 function CadastroVideo (){
     return(
         <PageDefault>
-            Pagina para cadastro de video
+
+            <h1>Cadastro de Video</h1>
+
+            <Link to='/cadastro/categorias'>
+                Cadastrar nova categoria 
+            </Link>
+
         </PageDefault>
     )
 }
